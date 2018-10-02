a6d700d5adf943ad52f9f3bdd943213a5787b3fd
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2018-10-02 18:19:00 +0200

    gaus fits work (for L now)

--- analysis/calibrateTreeLite.cpp
+++ analysis/calibrateTreeLite.cpp
@@ -12,7 +12,9 @@ bool do_tDiff = false;
 
 
 
-void fitLandau( const std::string& plotDir, TTree* tree, TH1D* histo, const std::string& varName );
+TF1* fitLandau( const std::string& outdir, TTree* tree, TH1D* histo, const std::string& varName );
+TF1* fitGaus( const std::string& outdir, TH1D* histo );
+std::vector<float> getBins( int nBins, float xMin, float xMax );
 
 
 
@@ -53,13 +55,22 @@ int main( int argc, char* argv[] ) {
   TH1D* h1_ampMaxL = new TH1D( "ampMaxL", "", 100, 0.1 , 0.8  );
   TH1D* h1_ampMaxR = new TH1D( "ampMaxR", "", 100, 0., 0.25 );
 
-  fitLandau( outdir, tree, h1_ampMaxL, "ampMaxL" );
-  fitLandau( outdir, tree, h1_ampMaxR, "ampMaxR" );
+  TF1* fitLandauL = fitLandau( outdir, tree, h1_ampMaxL, "ampMaxL" );
+  TF1* fitLandauR = fitLandau( outdir, tree, h1_ampMaxR, "ampMaxR" );
 
-  float ampMaxL_min, ampMaxL_max;
-  float ampMaxR_min, ampMaxR_max;
+  float ampMaxL_maxCut  = fitLandauL->GetParameter(1)*3.;
+  float ampMaxL_minCut  = fitLandauL->GetParameter(1)*0.8;
+
+  float ampMaxR_maxCut  = fitLandauR->GetParameter(1)*3.;
+  float ampMaxR_minCut  = fitLandauR->GetParameter(1)*0.8;
+
+  float ampMaxL_maxBins = fitLandauL->GetParameter(1)*2.;
+  float ampMaxL_minBins = fitLandauL->GetParameter(1)*0.8;
+
+  float ampMaxR_maxBins = fitLandauR->GetParameter(1)*2.;
+  float ampMaxR_minBins = fitLandauR->GetParameter(1)*0.8;
+
   
-  exit(1);
   std::string suffix = "";
   if( do_ampWalk ) suffix = suffix + "_AW";
   if( do_tDiff ) suffix = suffix + "_TD";
@@ -71,34 +82,23 @@ int main( int argc, char* argv[] ) {
   float tRcorr;
   newTree->Branch( "tRcorr", &tRcorr );
 
-
-  std::vector<float> bins_ampMax;
-  bins_ampMax.push_back(0.1);
-  bins_ampMax.push_back(0.2);
-  bins_ampMax.push_back(0.3);
-  bins_ampMax.push_back(0.4);
-  bins_ampMax.push_back(0.5);
-  bins_ampMax.push_back(0.6);
-  bins_ampMax.push_back(0.7);
-  bins_ampMax.push_back(0.8);
-  bins_ampMax.push_back(0.9);
-  bins_ampMax.push_back(1.0);
+  int nBins_ampMax = 10;
+  std::vector<float> bins_ampMaxL = getBins( nBins_ampMax, ampMaxL_minBins, ampMaxL_maxBins );
+  std::vector<float> bins_ampMaxR = getBins( nBins_ampMax, ampMaxR_minBins, ampMaxL_maxBins );
 
   std::vector< TH1D* > vh1_tL;
   std::vector< TH1D* > vh1_ampMaxL;
 
-  for( unsigned i=0; i<bins_ampMax.size()-1; ++i ) {
+  for( unsigned i=0; i<bins_ampMaxL.size()-1; ++i ) {
 
-    TH1D* h1_tL = new TH1D( Form("tL_bin%d", i), "", 50, 0., 3. );
+    TH1D* h1_tL = new TH1D( Form("tL_bin%d", i), "", 100, 2., 4. );
     vh1_tL.push_back( h1_tL );
 
-    TH1D* h1_ampMaxL = new TH1D( Form("ampMaxL_bin%d", i), "", 50, bins_ampMax[i], bins_ampMax[i+1] );
+    TH1D* h1_ampMaxL = new TH1D( Form("ampMaxL_bin%d", i), "", 50, bins_ampMaxL[i], bins_ampMaxL[i+1] );
     vh1_ampMaxL.push_back( h1_ampMaxL );
 
   } // for bins_ampMax
 
-  float ampMaxMin = vh1_ampMaxL[0]->GetXaxis()->GetXmin();
-  float ampMaxMax = vh1_ampMaxL[vh1_ampMaxL.size()-1]->GetXaxis()->GetXmax();
 
   int nentries = tree->GetEntries();
 
@@ -109,17 +109,17 @@ int main( int argc, char* argv[] ) {
 
     tree->GetEntry( iEntry );
 
-    if( ampMaxL>=ampMaxMin && ampMaxL<=ampMaxMax ) {  // ampMaxL is good
+    if( ampMaxL>=ampMaxL_minCut && ampMaxL<=ampMaxL_maxCut ) {  // ampMaxL is good
 
       int thisBinL = -1;
-      for( unsigned i=0; i<bins_ampMax.size()-1; ++i ) {
-        if( ampMaxL>bins_ampMax[i] && ampMaxL<bins_ampMax[i+1] ) {
+      for( unsigned i=0; i<bins_ampMaxL.size()-1; ++i ) {
+        if( ampMaxL>bins_ampMaxL[i] && ampMaxL<bins_ampMaxL[i+1] ) {
           thisBinL = i;
           break;
         } 
       }
 
-      if( thisBinL<0 ) std::cout << "THIS SHOULDN'T BE POSSIBLE" << std::endl;
+      if( thisBinL<0 ) continue;
 
       vh1_tL[thisBinL]->Fill( tL );
       vh1_ampMaxL[thisBinL]->Fill( ampMaxL );
@@ -129,13 +129,42 @@ int main( int argc, char* argv[] ) {
   } // for entries
 
 
+  TGraphErrors* gr_ampWalkL = new TGraphErrors(0);
+
+  std::string fitsDir(Form("%s/ampWalkFits", outdir.c_str()));
+  system( Form("mkdir -p %s", fitsDir.c_str()) );
+
+  for( unsigned i=0; i<vh1_tL.size(); ++i ) {
+
+    TF1* f1_gausL = fitGaus( fitsDir, vh1_tL[i] );
+
+    float xL     = vh1_ampMaxL[i]->GetMean();
+    float xL_err = vh1_ampMaxL[i]->GetMeanError();
+
+    float yL     = f1_gausL->GetParameter( 1 );
+    float yL_err = f1_gausL->GetParError ( 1 );
+
+    int iL = gr_ampWalkL->GetN();
+    gr_ampWalkL->SetPoint     ( iL, xL    , yL     );
+    gr_ampWalkL->SetPointError( iL, xL_err, yL_err );
+
+  } // for points
+
+
+  outfile->cd();
+
+  for( unsigned i=0; i<vh1_tL     .size(); ++i ) vh1_tL[i]     ->Write();
+  for( unsigned i=0; i<vh1_ampMaxL.size(); ++i ) vh1_ampMaxL[i]->Write();
+
+  outfile->Close();
+
   return 0;
 
 }
 
 
 
-void fitLandau( const std::string& plotDir, TTree* tree, TH1D* histo, const std::string& varName ) {
+TF1* fitLandau( const std::string& outdir, TTree* tree, TH1D* histo, const std::string& varName ) {
 
   tree->Project( histo->GetName(), varName.c_str(), "" );
 
@@ -143,21 +172,18 @@ void fitLandau( const std::string& plotDir, TTree* tree, TH1D* histo, const std:
   float xMin_fit = xMode*0.8;
   float xMax_fit = xMode*3.;
 
-  TF1* f1_landau0 = new TF1( Form("landau0_%s", varName.c_str()), "landau", xMin_fit, xMax_fit );
+  TF1* f1_landau = new TF1( Form("landau_%s", varName.c_str()), "landau", xMin_fit, xMax_fit );
   
-  histo->Fit( f1_landau0->GetName(), "R0" );
+  histo->Fit( f1_landau->GetName(), "R0" );
 
-  xMin_fit = 0.9*f1_landau0->GetParameter(1);
-  xMax_fit = 1.4*f1_landau0->GetParameter(1);
+  xMin_fit = 0.9*f1_landau->GetParameter(1);
+  xMax_fit = 1.4*f1_landau->GetParameter(1);
+
+  f1_landau->SetRange( xMin_fit, xMax_fit );
 
   int n_iter = 5;
 
-  TF1* f1_landau;
-
   for( unsigned i=0; i<n_iter; ++i ) { // iterative fit
-
-    f1_landau = new TF1( Form("landau_%s", varName.c_str()), "landau", xMin_fit, xMax_fit );
-
 
     if( i==n_iter-1 )
       histo->Fit( f1_landau->GetName(), "R+" );
@@ -165,7 +191,7 @@ void fitLandau( const std::string& plotDir, TTree* tree, TH1D* histo, const std:
       histo->Fit( f1_landau->GetName(), "R0" );
       xMin_fit = 0.9*f1_landau->GetParameter(1);
       xMax_fit = 1.4*f1_landau->GetParameter(1);
-      delete f1_landau;
+      f1_landau->SetRange( xMin_fit, xMax_fit );
     }
 
   } // for iter
@@ -175,9 +201,70 @@ void fitLandau( const std::string& plotDir, TTree* tree, TH1D* histo, const std:
 
   histo->Draw();
 
-  c1->SaveAs( Form("%s/%s.eps", plotDir.c_str(), histo->GetName()) );
-  c1->SaveAs( Form("%s/%s.pdf", plotDir.c_str(), histo->GetName()) );
+  c1->SaveAs( Form("%s/%s.eps", outdir.c_str(), histo->GetName()) );
+  c1->SaveAs( Form("%s/%s.pdf", outdir.c_str(), histo->GetName()) );
 
-  return;
+  return f1_landau;
+
+}
+
+
+std::vector<float> getBins( int nBins, float xMin, float xMax ) {
+
+  float step = (xMax-xMin)/((float)nBins);
+
+  std::vector<float> bins;
+
+  for( unsigned i=0; i<nBins; ++i )
+    bins.push_back( xMin + (float)i*step );
+
+  return bins;
+
+}
+
+
+
+TF1* fitGaus( const std::string& outdir, TH1D* histo ) {
+
+  float mean_histo = histo->GetMean();
+  float rms_histo  = histo->GetRMS();
+
+  TF1* f1_gaus = new TF1( Form("gaus_%s", histo->GetName()), "gaus", mean_histo-rms_histo, mean_histo+rms_histo );
+  
+  histo->Fit( f1_gaus->GetName(), "R0" );
+
+  float nSigma = 2.5;
+  float xMin_fit = f1_gaus->GetParameter(1) - nSigma*f1_gaus->GetParameter(2);
+  float xMax_fit = f1_gaus->GetParameter(1) + nSigma*f1_gaus->GetParameter(2);
+
+  f1_gaus->SetRange( xMin_fit, xMax_fit );
+
+  //std::cout << "Range: " << xMin_fit << " " << xMax_fit << std::endl;
+
+  int n_iter = 5;
+
+  for( unsigned i=0; i<n_iter; ++i ) { // iterative fit
+
+    if( i==n_iter-1 )
+      histo->Fit( f1_gaus->GetName(), "R+" );
+    else {
+      histo->Fit( f1_gaus->GetName(), "RQ0" );
+      xMin_fit = f1_gaus->GetParameter(1) - nSigma*f1_gaus->GetParameter(2);
+      xMax_fit = f1_gaus->GetParameter(1) + nSigma*f1_gaus->GetParameter(2);
+      f1_gaus->SetRange( xMin_fit, xMax_fit );
+  //std::cout << "Range: " << xMin_fit << " " << xMax_fit << std::endl;
+    }
+
+  } // for iter
+
+  TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
+  c1->cd();
+
+  histo->Draw();
+
+  c1->SaveAs( Form("%s/%s.eps", outdir.c_str(), histo->GetName()) );
+  c1->SaveAs( Form("%s/%s.pdf", outdir.c_str(), histo->GetName()) );
+
+  return f1_gaus;
 
 }
