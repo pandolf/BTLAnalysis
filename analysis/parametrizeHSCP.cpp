f647f63225fd7a57e4b87b1a7fbc78b467794afb
Francesco Pandolfi <pandolf@dhwired13015.roma1.infn.it>
2019-03-11 15:42:59 +0100

    added EE

--- analysis/parametrizeHSCP.cpp
+++ analysis/parametrizeHSCP.cpp
@@ -12,6 +12,8 @@
 
 void addPointToGraph( TGraphErrors* graph, float par, float parerr, float x );
 TF1* fitGraph( const std::string& outdir, const std::string& name, TGraphErrors* graph, float xMin, float xMax );
+TH1D* removeErrorZeroBins( TProfile* thisProfile );
+void saveParamToFile( TFile* file, const std::string& outdir, TTree* tree, const std::string& name, const std::vector<float> etaBins );
 
 
 int main() {
@@ -22,11 +24,24 @@ int main() {
   TFile* file = TFile::Open( "hscpLite_DY_crab_yesMTD.root" );
   TTree* tree = (TTree*)file->Get("treeLite");
 
+  std::string outdir( "fitsHSCP" );
+  system( Form("mkdir -p %s", outdir.c_str()) );
 
-  std::vector<float> etaBins;
-  for( float eta=1.55; eta<3.; eta+=0.05 )
-    etaBins.push_back( eta );
+  TFile* outfile = TFile::Open( "paramFileHSCP.root", "recreate" );
 
+  std::vector<float> etaBinsEB;
+  for( float eta=0.; eta<1.4; eta+=0.05 )
+    etaBinsEB.push_back( eta );
+
+  saveParamToFile( outfile, outdir, tree, "eb", etaBinsEB );
+
+  std::vector<float> etaBinsEE;
+  for( float eta=1.55; eta<2.95; eta+=0.05 )
+    etaBinsEE.push_back( eta );
+
+  saveParamToFile( outfile, outdir, tree, "ee", etaBinsEE );
+
+  outfile->Close();
 
   //etaBins.push_back(0.);
   //etaBins.push_back(0.1);
@@ -44,63 +59,72 @@ int main() {
   //etaBins.push_back(1.3);
   //etaBins.push_back(1.4);
 
-  float xMin = 0.7;
-  float xMax = 4.7;
 
-  std::string outdir( "fitsHSCP" );
-  system( Form("mkdir -p %s", outdir.c_str()) );
+return 0;
+
+}
+
+
+
+void saveParamToFile( TFile* file, const std::string& outdir, TTree* tree, const std::string& name, const std::vector<float> etaBins ) {
+
+  bool isEB = (fabs(etaBins[0]) < 1.4);
+
+  float ptMin = 0.7;
+  float ptMax = 4.7;
 
   TGraphErrors* gr_p0 = new TGraphErrors(0);
-  gr_p0->SetName("p0");
+  gr_p0->SetName(Form("p0_%s", name.c_str()));
 
   TGraphErrors* gr_p1 = new TGraphErrors(0);
-  gr_p1->SetName("p1");
+  gr_p1->SetName(Form("p1_%s", name.c_str()));
 
   TGraphErrors* gr_p2 = new TGraphErrors(0);
-  gr_p2->SetName("p2");
+  gr_p2->SetName(Form("p2_%s", name.c_str()));
 
 
   for( unsigned iEta=0; iEta<etaBins.size()-1; ++iEta ) {
 
-    if( iEta==8 ) continue; // for now
+    TProfile* thisProfile = new TProfile( Form("prof_%s_eta%d", name.c_str(), iEta), "", 200, ptMin, ptMax );
 
-    TProfile* thisProfile = new TProfile( Form("prof_eta%d", iEta), "", 200, xMin, xMax );
-
-    std::string selection(Form("abs(eta)>%f && abs(eta)<%f && pt < %f && pathLength>20.", etaBins[iEta], etaBins[iEta+1], xMax));
+    std::string selection(Form("abs(eta)>%f && abs(eta)<%f && pt < %f && pathLength>20.", etaBins[iEta], etaBins[iEta+1], ptMax));
     tree->Project( thisProfile->GetName(), "pathLength:pt", selection.c_str(), "prof");
 
-    float xMin_fit = 0.82;
-    TF1* thisFunc = new TF1( Form("func_eta%d", iEta), "[0] + [1]/x + [2]/x/x",  xMin_fit, xMax );
-    thisFunc->SetParameter( 0, thisProfile->GetBinContent(150) );
+    TH1D* cleanProfile = removeErrorZeroBins( thisProfile );
+
+    float ptMin_fit = (isEB) ? 0.82 : 1.;
+    TF1* thisFunc = new TF1( Form("func_%s_eta%d", name.c_str(), iEta), "[0] + [1]/x + [2]/x/x",  ptMin_fit, ptMax );
+    thisFunc->SetParameter( 0, cleanProfile->GetBinContent(150) );
     thisFunc->SetLineColor(46);
 
-    thisProfile->Fit( thisFunc, "QR" );
+    cleanProfile->Fit( thisFunc, "QR" );
 
-    TCanvas* c1 = new TCanvas( Form("c1_%d", iEta), "", 600, 600 );
+    TCanvas* c1 = new TCanvas( Form("c1_%s_%d", name.c_str(), iEta), "", 600, 600 );
     c1->cd();
 
-    TH2D* h2_axes = new TH2D( Form("axes_eta%d", iEta), "", 10, xMin, xMax, 10, 0.9*thisFunc->Eval(0.8*xMax), 1.4*thisFunc->Eval(0.8*xMax) );
+    float scaleYmax = (isEB) ? 1.4 : 1.05;
+    TH2D* h2_axes = new TH2D( Form("axes_%s_eta%d", name.c_str(), iEta), "", 10, ptMin, ptMax, 10, 0.90*thisFunc->Eval(0.8*ptMax), scaleYmax*thisFunc->Eval(0.8*ptMax) );
     h2_axes->SetXTitle( "Track p_{T} [GeV]" );
     h2_axes->SetYTitle( "Path Length [cm]" );
     h2_axes->Draw();
 
-    thisProfile->SetMarkerStyle(20);
-    thisProfile->SetMarkerColor(kBlack);
-    thisProfile->Draw("P same");
+    cleanProfile->SetMarkerStyle(20);
+    cleanProfile->SetMarkerColor(kBlack);
+    cleanProfile->Draw("P same");
 
     TPaveText* etaLabel = new TPaveText( 0.7, 0.8, 0.9, 0.9, "brNDC" );
     etaLabel->SetFillColor(0);
     etaLabel->SetTextSize(0.035);
-    etaLabel->AddText( Form("%.1f < |#eta| < %.1f", etaBins[iEta], etaBins[iEta+1]) );
+    etaLabel->AddText( Form("%.2f < |#eta| < %.2f", etaBins[iEta], etaBins[iEta+1]) );
     etaLabel->Draw("same");
 
     gPad->RedrawAxis();
 
-    c1->SaveAs( Form("%s/fit_eta%d.pdf", outdir.c_str(), iEta) );
+    c1->SaveAs( Form("%s/fit_%s_eta%d.pdf", outdir.c_str(), name.c_str(), iEta) );
 
 
-    TH1D* h1_eta = new TH1D( Form("eta_eta%d", iEta), "", 200, 0., 2.5 );
-    tree->Project( h1_eta->GetName(), "eta", selection.c_str() );
+    TH1D* h1_eta = new TH1D( Form("eta_%s_eta%d", name.c_str(), iEta), "", 200, 0., 3. );
+    tree->Project( h1_eta->GetName(), "abs(eta)", selection.c_str() );
 
     addPointToGraph( gr_p0, thisFunc->GetParameter(0), thisFunc->GetParError(0), h1_eta->GetMean() );
     addPointToGraph( gr_p1, thisFunc->GetParameter(1), thisFunc->GetParError(1), h1_eta->GetMean() );
@@ -110,24 +134,19 @@ int main() {
     delete h2_axes;
     delete h1_eta;
     delete thisProfile;
+    delete cleanProfile;
 
   } // for eta bins 
 
-  TF1* f1_p0 = fitGraph( outdir, "f1_p0", gr_p0, etaBins[0], etaBins[etaBins.size()-1] );
-  TF1* f1_p1 = fitGraph( outdir, "f1_p1", gr_p1, etaBins[0], etaBins[etaBins.size()-1] );
-  TF1* f1_p2 = fitGraph( outdir, "f1_p2", gr_p2, etaBins[0], etaBins[etaBins.size()-1] );
+  TF1* f1_p0 = fitGraph( outdir, Form("f1_%s_p0", name.c_str()), gr_p0, etaBins[0], etaBins[etaBins.size()-1] );
+  TF1* f1_p1 = fitGraph( outdir, Form("f1_%s_p1", name.c_str()), gr_p1, etaBins[0], etaBins[etaBins.size()-1] );
+  TF1* f1_p2 = fitGraph( outdir, Form("f1_%s_p2", name.c_str()), gr_p2, etaBins[0], etaBins[etaBins.size()-1] );
 
-
-  TFile* outfile = TFile::Open( "paramFileHSCP.root", "recreate" );
-  outfile->cd();
+  file->cd();
 
   f1_p0->Write();
   f1_p1->Write();
   f1_p2->Write();
-
-  outfile->Close();
-
-  return 0;
 
 }
 
@@ -161,5 +180,25 @@ TF1* fitGraph( const std::string& outdir, const std::string& name, TGraphErrors*
   delete c1;
 
   return f1;
+
+}
+
+
+TH1D* removeErrorZeroBins( TProfile* thisProfile ) {
+
+  TH1D* newProfile = new TH1D( Form("nozero_%s", thisProfile->GetName()), "", thisProfile->GetNbinsX(), thisProfile->GetXaxis()->GetXmin(), thisProfile->GetXaxis()->GetXmax() );
+
+  for( unsigned i=1; i<thisProfile->GetNbinsX()+1; i++ ) {
+
+    if( i>1 && (thisProfile->GetBinError(i) > 0.1*thisProfile->GetBinError(i-1)) ) {  // for stability
+
+      newProfile->SetBinContent( i, thisProfile->GetBinContent(i) );
+      newProfile->SetBinError  ( i, thisProfile->GetBinError(i)   );
+
+    } // if
+
+  } // for bins
+
+  return newProfile;
 
 }
