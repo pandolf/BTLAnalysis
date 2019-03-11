f6277e4cdc5361b6aae77a0c17505c2e8afac0ff
Francesco Pandolfi <pandolf@dhwired13015.roma1.infn.it>
2019-03-11 12:49:04 +0100

    shaded version of pt max plot

--- analysis/drawHSCP.cpp
+++ analysis/drawHSCP.cpp
@@ -12,7 +12,8 @@
 
 
 
-float getSigmaMass( float mass, float pt, float eta, TF1* f1_p0, TF1* f1_p1, TF1* f1_p2 );
+float getSigmaMass( float sigma_t, float mass, float pt, float eta, TF1* f1_p0, TF1* f1_p1, TF1* f1_p2 );
+float findPtMax( float mX, float sigma_t, float eta, TF1* f1_p0, TF1* f1_p1, TF1* f1_p2 );
 
 
 int main( int argc, char* argv[] ) {
@@ -27,17 +28,32 @@ int main( int argc, char* argv[] ) {
   TF1* f1_p2 = (TF1*)paramFile->Get("f1_p2");
 
   std::vector<float> etaValues;
-  etaValues.push_back(0.);
-  etaValues.push_back(0.3);
-  etaValues.push_back(0.6);
-  etaValues.push_back(1.0);
-  etaValues.push_back(1.3);
+  for( float eta=0.; eta<1.4; eta += 0.04 )
+    etaValues.push_back(eta);
+
+
+
 
 
   std::vector<TGraph*> graphs;
 
-  //float mass = 0.983;
-  float mass = 100.;
+  TGraph* gr_ptMax_vs_eta_30ps = new TGraph(0);
+  TGraph* gr_ptMax_vs_eta_50ps = new TGraph(0);
+  TGraph* gr_ptMax_vs_eta_70ps = new TGraph(0);
+
+  gr_ptMax_vs_eta_30ps->SetName("gr_ptMax_vs_eta_30ps");
+  gr_ptMax_vs_eta_50ps->SetName("gr_ptMax_vs_eta_50ps");
+  gr_ptMax_vs_eta_70ps->SetName("gr_ptMax_vs_eta_70ps");
+
+  // first point on x axis (for fill):
+  gr_ptMax_vs_eta_30ps->SetPoint( 0, 0., 0. );
+  gr_ptMax_vs_eta_50ps->SetPoint( 0, 0., 0. );
+  gr_ptMax_vs_eta_70ps->SetPoint( 0, 0., 0. );
+
+
+  float mass = 0.983;
+  float mX = 500.;
+  //float mass = 0.139;
 
   for( unsigned iEta=0; iEta<etaValues.size(); ++iEta ) {
 
@@ -45,11 +61,22 @@ int main( int argc, char* argv[] ) {
     gr_sigmam_vs_pt->SetName( Form("gr_sigmam_vs_pt_eta%d", iEta) );
 
     for( float pt=1.; pt<50.; pt+=0.5 )
-      gr_sigmam_vs_pt->SetPoint( gr_sigmam_vs_pt->GetN(), pt, getSigmaMass( mass, pt, etaValues[iEta], f1_p0, f1_p1, f1_p2 ) );
+      gr_sigmam_vs_pt->SetPoint( gr_sigmam_vs_pt->GetN(), pt, mass + 3.*getSigmaMass( 0.030, mass, pt, etaValues[iEta], f1_p0, f1_p1, f1_p2 ) );
 
     graphs.push_back( gr_sigmam_vs_pt );
 
+
+    gr_ptMax_vs_eta_30ps->SetPoint( gr_ptMax_vs_eta_30ps->GetN(), etaValues[iEta], findPtMax( mX, 0.030, etaValues[iEta], f1_p0, f1_p1, f1_p2 ) );
+    gr_ptMax_vs_eta_50ps->SetPoint( gr_ptMax_vs_eta_50ps->GetN(), etaValues[iEta], findPtMax( mX, 0.050, etaValues[iEta], f1_p0, f1_p1, f1_p2 ) );
+    gr_ptMax_vs_eta_70ps->SetPoint( gr_ptMax_vs_eta_70ps->GetN(), etaValues[iEta], findPtMax( mX, 0.070, etaValues[iEta], f1_p0, f1_p1, f1_p2 ) );
+
   } // for ieta
+
+  // add last point on x axis so that fill drawing works:
+  gr_ptMax_vs_eta_30ps->SetPoint( gr_ptMax_vs_eta_30ps->GetN(), etaValues[etaValues.size()-1], 0. );
+  gr_ptMax_vs_eta_50ps->SetPoint( gr_ptMax_vs_eta_50ps->GetN(), etaValues[etaValues.size()-1], 0. );
+  gr_ptMax_vs_eta_70ps->SetPoint( gr_ptMax_vs_eta_70ps->GetN(), etaValues[etaValues.size()-1], 0. );
+
 
   TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
   c1->cd();
@@ -70,7 +97,7 @@ int main( int argc, char* argv[] ) {
     graphs[i]->SetMarkerStyle(20);
     graphs[i]->SetMarkerSize(1.3);
     graphs[i]->SetMarkerColor(colors[i]);
-    graphs[i]->Draw("p same");
+    graphs[i]->Draw("P same");
 
     legend->AddEntry( graphs[i], Form("|#eta| = %.1f", etaValues[i]), "P" );
 
@@ -82,22 +109,85 @@ int main( int argc, char* argv[] ) {
 
   c1->SaveAs("hscp_sigmam_vs_pt.pdf");
 
-  delete c1;
   delete h2_axes;
 
 
-TFile* outfile = TFile::Open("prova.root", "recreate");
-outfile->cd();
-  for( unsigned i=0; i<graphs.size(); ++i ) 
-    graphs[i]->Write();
-outfile->Close();
+  c1->Clear();
+
+  c1->cd();
+
+
+  TH2D* h2_axes2 = new TH2D("axes2", "", 10, 0., 1.5, 10, 0., 190.);
+  h2_axes2->SetXTitle( "|#eta|" );
+  h2_axes2->SetYTitle( "Maximum p_{T} [GeV]" );
+  h2_axes2->Draw();
+
+  
+  gr_ptMax_vs_eta_30ps->SetMarkerStyle(20);
+  gr_ptMax_vs_eta_30ps->SetMarkerSize(1.5);
+  gr_ptMax_vs_eta_30ps->SetMarkerColor(colors[0]);
+  gr_ptMax_vs_eta_30ps->SetFillColor(colors[0]);
+  
+  gr_ptMax_vs_eta_50ps->SetMarkerStyle(20);
+  gr_ptMax_vs_eta_50ps->SetMarkerSize(1.5);
+  gr_ptMax_vs_eta_50ps->SetMarkerColor(colors[1]);
+  gr_ptMax_vs_eta_50ps->SetFillColor(colors[1]);
+
+  gr_ptMax_vs_eta_70ps->SetMarkerStyle(20);
+  gr_ptMax_vs_eta_70ps->SetMarkerSize(1.5);
+  gr_ptMax_vs_eta_70ps->SetMarkerColor(colors[2]);
+  gr_ptMax_vs_eta_70ps->SetFillColor(colors[2]);
+
+  //TLegend* legend2 = new TLegend( 0.2, 0.2, 0.5, 0.35 );
+  //legend2->SetTextSize(0.035);
+  //legend2->SetFillColor(0);
+  //legend2->AddEntry( gr_ptMax_vs_eta_30ps, "#sigma_{t} = 30 ps", "P" );
+  //legend2->AddEntry( gr_ptMax_vs_eta_50ps, "#sigma_{t} = 50 ps", "P" );
+  //legend2->AddEntry( gr_ptMax_vs_eta_70ps, "#sigma_{t} = 70 ps", "P" );
+  //legend2->Draw("same");
+
+  gr_ptMax_vs_eta_30ps->Draw("F same" );
+  gr_ptMax_vs_eta_50ps->Draw("F same" );
+  gr_ptMax_vs_eta_70ps->Draw("F same" );
+
+  TPaveText* label_70ps = new TPaveText( 0.05, 80., 0.35, 90. );
+  label_70ps->SetFillColor(colors[2]);
+  label_70ps->SetTextSize(0.035);
+  label_70ps->AddText( "#sigma_{t} = 70 ps" );
+  label_70ps->Draw("same");
+
+  TPaveText* label_50ps = new TPaveText( 0.05, 100., 0.35, 110. );
+  label_50ps->SetFillColor(colors[1]);
+  label_50ps->SetTextSize(0.035);
+  label_50ps->AddText( "#sigma_{t} = 50 ps" );
+  label_50ps->Draw("same");
+
+  TPaveText* label_30ps = new TPaveText( 0.05, 135., 0.35, 140. );
+  label_30ps->SetFillColor(colors[0]);
+  label_30ps->SetTextSize(0.035);
+  label_30ps->AddText( "#sigma_{t} = 30 ps" );
+  label_30ps->Draw("same");
+
+
+  TPaveText* label_hscp = new TPaveText( 0.6, 0.8, 0.9, 0.9, "brNDC" );
+  label_hscp->SetFillColor(0);
+  label_hscp->SetTextSize(0.04);
+  label_hscp->AddText( "HSCP M = 500 GeV");
+  label_hscp->Draw("same");
+
+  HSCPCommon::addLabels(c1);
+
+  c1->SaveAs( "ptMax_vs_eta.pdf" );
+
+  delete c1;
+
 
   return 0;
 
 }
 
 
-float getSigmaMass( float mass, float pt, float eta, TF1* f1_p0, TF1* f1_p1, TF1* f1_p2 ) {
+float getSigmaMass( float sigma_t, float mass, float pt, float eta, TF1* f1_p0, TF1* f1_p1, TF1* f1_p2 ) {
 
   TLorentzVector* p4 = new TLorentzVector();
   p4->SetPtEtaPhiM( pt, eta, 0., mass );
@@ -111,9 +201,30 @@ float getSigmaMass( float mass, float pt, float eta, TF1* f1_p0, TF1* f1_p1, TF1
 
   float beta = p4->P()/p4->Energy();
 
-  float sigma_invBeta = 0.035 * 30./pathLength;
+  float sigma_invBeta = sigma_t * 30./pathLength;
   float sigma_m = p4->P()*sigma_invBeta/( beta*sqrt( 1./(beta*beta) - 1. ) );
 
   return sigma_m;
+
+}
+
+
+
+float findPtMax( float mX, float sigma_t, float eta, TF1* f1_p0, TF1* f1_p1, TF1* f1_p2 ) {
+
+  float ptMax = -1.;
+
+  for( float pt=1.; pt<1000.; pt += 0.5 ) {
+
+    float ul = 0.938 + 3.*getSigmaMass( sigma_t, 0.983, pt, eta, f1_p0, f1_p1, f1_p2 );
+
+    if( ul >= mX ) {
+      ptMax = pt;
+      break;
+    }
+
+  } // for pt;
+
+  return ptMax;
 
 }
